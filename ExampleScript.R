1496494632df6a0877a91b58568690c37fbf774a
ambro <ambrosio2112.data@gmail.com>
2020-08-30 19:33:24 -0500

    Add Cyl vs Gear table

--- ExampleScript.R
+++ ExampleScript.R
@@ -5,9 +5,12 @@ require(tidyverse)
 data("mtcars")
 
 mtcars %>% View()
+
 mtcars %>% ggplot()+
   geom_point(aes(x = hp,
                  y = mpg,
                  size = cyl,
                  color = gear))+
   ggtitle("Mpg vs hp")
+
+table(mtcars$cyl, mtcars$gear)
